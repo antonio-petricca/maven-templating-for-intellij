a2969f42a79db6b61927be32761df315f25874b1
Antonio Petricca <antonio.petricca@gmail.com>
2022-01-12 12:14:33 +0100

    Managed templates folder removal.

--- src/main/kotlin/com/github/antoniopetricca/maventemplatingforintellij/listeners/MyVFSListener.kt
+++ src/main/kotlin/com/github/antoniopetricca/maventemplatingforintellij/listeners/MyVFSListener.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.newvfs.BulkFileListener
 import com.intellij.openapi.vfs.newvfs.events.VFileCreateEvent
+import com.intellij.openapi.vfs.newvfs.events.VFileDeleteEvent
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import org.jetbrains.jps.model.java.JavaSourceRootType.*
 
@@ -22,13 +23,16 @@ import org.jetbrains.jps.model.java.JavaSourceRootType.*
     https://plugins.jetbrains.com/docs/intellij/module.html
  */
 
-//TODO Make it configurable
+//TODO Make it configurable (activation, folder)
 //TODO Implement for moving and renaming
-//TODO Handle folder removal
+//TODO Check java project type
+//TODO IDE logging
 
 internal class MyVFSListener : BulkFileListener {
 
-    private val SOURCE_FOLDER = "java-templates"
+    private val TEMPLATES_FOLDER = "java-templates"
+    private val SOURCES_FOLDER   = "main/${TEMPLATES_FOLDER}"
+    private val TESTS_FOLDER     = "test/${TEMPLATES_FOLDER}"
 
     private fun addSourceFolder(virtualFile: VirtualFile?, isTestFolder: Boolean) {
         ProjectManager
@@ -43,8 +47,8 @@ internal class MyVFSListener : BulkFileListener {
                         .getInstance(module)
                         .modifiableModel
 
-                    var contentEntries = model.contentEntries
-                    val contentEntry   = (if (contentEntries.isEmpty()) model.addContentEntry(virtualFile) else contentEntries[0] )
+                    val contentEntries = model.contentEntries
+                    val contentEntry   = (if (contentEntries.isEmpty()) model.addContentEntry(virtualFile) else contentEntries[0])
                     val sourceRoots    = model.getSourceRoots(if (!isTestFolder) SOURCE else TEST_SOURCE)
 
                     if (!sourceRoots.contains(virtualFile)) {
@@ -55,23 +59,69 @@ internal class MyVFSListener : BulkFileListener {
             }
     }
 
+    private fun removeSourceFolder(virtualFile: VirtualFile?) {
+        ProjectManager
+            .getInstance()
+            .openProjects
+            .forEach { it ->
+                val fileIndex = ProjectFileIndex.SERVICE.getInstance(it)
+                val module    = fileIndex.getModuleForFile(virtualFile!!)
+
+                if (null != module) {
+                    val model = ModuleRootManager
+                        .getInstance(module)
+                        .modifiableModel
+
+                    val contentEntries = model.contentEntries
+                    val contentEntry   = (if (contentEntries.isEmpty()) model.addContentEntry(virtualFile) else contentEntries[0])
+
+                    contentEntry
+                        .sourceFolders
+                        .filter { it.file?.equals(virtualFile) ?: false }
+                        .forEach {
+                            contentEntry.removeSourceFolder(it)
+                            model.commit()
+                        }
+                }
+            }
+    }
+
     override fun after(events: MutableList<out VFileEvent>) {
         events.forEach {
+            val path           = it.path;
+            val isSourceFolder = path.contains(SOURCES_FOLDER)
+            val isTestFolder   = path.contains(TESTS_FOLDER)
+
             if (it is VFileCreateEvent) {
-                val createEvent = it as VFileCreateEvent
+                val createEvent = it
 
-                if (createEvent.isDirectory) {
-                    val path           = createEvent.path;
-                    val isSourceFolder = path.contains("main/${SOURCE_FOLDER}")
-                    val isTestFolder   = path.contains("test/${SOURCE_FOLDER}")
-
-                    if (isSourceFolder || isTestFolder) {
-                        addSourceFolder(createEvent.file, isTestFolder)
-                    }
+                if (
+                       createEvent.isDirectory
+                    && (isSourceFolder || isTestFolder)
+                ) {
+                    addSourceFolder(createEvent.file, isTestFolder)
                 }
             }
         }
 
         super.after(events)
+    }
+
+    override fun before(events: MutableList<out VFileEvent>) {
+        events.forEach {
+            val path           = it.path;
+            val isSourceFolder = path.contains(SOURCES_FOLDER)
+            val isTestFolder   = path.contains(TESTS_FOLDER)
+
+            if (it is VFileDeleteEvent) {
+                val deleteEvent = it
+
+                if (isSourceFolder || isTestFolder) {
+                    removeSourceFolder(deleteEvent.file)
+                }
+            }
+        }
+
+        super.before(events)
     }
 }
