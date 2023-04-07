8cdf84de4cb78317744fc2ace88e43b359ec6d59
Antonio Petricca <antonio.petricca@gmail.com>
2023-04-07 12:27:52 +0200

    Update: id( "org.jetbrains.intellij") version "1.13.3"

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.13"
     // Gradle IntelliJ Plugin
-    id( "org.jetbrains.intellij") version "1.10.0"
+    id( "org.jetbrains.intellij") version "1.13.3"
     kotlin("jvm") version "1.8.20-RC2"
 }
 
